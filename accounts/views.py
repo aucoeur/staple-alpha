20037915bb72bf4d0308ce3ace849f26c35bc263
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-12-09 10:25:50 -0800

    Fixed signup template location

--- accounts/views.py
+++ accounts/views.py
@@ -8,4 +8,4 @@ from django.views.generic import CreateView
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'accounts/signup.html'
+    template_name = 'templates/registration/signup.html'
