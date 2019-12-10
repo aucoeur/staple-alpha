fea821fb9da4eb7ccbdd9cd0898f5b1b5530538b
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-12-10 11:54:52 -0800

    Add document model

--- staple/urls.py
+++ staple/urls.py
@@ -5,5 +5,6 @@ from staple import views
 urlpatterns = [
     # path('', IndexView.as_view(), name = 'staple-index'),
     path('', views.index, name = 'staple-index'),
-    path('new', views.new_packet, name = 'new-packet')
+    path('new', views.new_packet, name = 'new-packet'),
+    # path('<str:slug>/', PageDetailView.as_view(), name='document-details-page'),
 ]
