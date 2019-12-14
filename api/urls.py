2cde4042faf97bb92c4e4da3516f8b0e904fedec
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-12-13 17:08:14 -0800

    fixed api

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from api.views import DocumentList, DocumentDetail
 
 urlpatterns = [
-    path('documents/', DocumentList.as_view(), name='documents_list'),
-    path('document/<str:slug>/', DocumentDetail.as_view(), name='document_detail')
+    path('', DocumentList.as_view(), name='documents_list'),
+    path('<str:slug>/', DocumentDetail.as_view(), name='document_detail')
 ]
