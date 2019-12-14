cdfe0b450619dc63aa3986efa7f3274d827184cb
aucoeur <aucoeur.ngo@students.makeschool.com>
2019-12-13 17:04:57 -0800

    Halfsies fixed API

--- api/views.py
+++ api/views.py
@@ -1,13 +1,18 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
 from staple.models import Document
 from api.serializers import DocumentSerializer
 
-class DocumentList(ListCreateAPIView):
-    queryset = Document.objects.all()
-    serializer_class = DocumentSerializer
+class DocumentList(APIView):
+    def get(self, request):
+        documents = Document.objects.all()[:20]
+        data = DocumentSerializer(documents, many=True).data
+        return Response(data)
 
-class DocumentDetail(RetrieveDestroyAPIView):
-    queryset = Document.objects.all()
-    serializer_class = DocumentSerializer
+class DocumentDetail(APIView):
+    def get(self, request, pk):
+        document = get_object_or_404(Document, pk=pk)
+        data = DocumentSerializer(document).data
+        return Response(data)
